f72047bf5f0f843372ce6b989a38f67ef1ff1365
Yahikko <taczuhiro@mail.ru>
2023-07-21 16:29:13 +0300

    Exercise: Horn of the unicorn

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,13 @@
 const val HERO_NAME = "Madrigal"
+const val BARTENDER_NAME = "John"
+const val TAVERN_NAME = "Horn Of The Unicorn"
 
 fun main() {
 
     var hasSteed = false
+    var goldCount = 50
+
+    val tavernMenu = mapOf("Honey" to 20, "Beer" to 15, "Wine" to 18)
 
     println("The hero announces her presence to the sword.")
 
