8b8bc06a386158f753e8c17ba8ed3a540c75ad0f
Yahikko <taczuhiro@mail.ru>
2023-07-21 16:09:32 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+
+const val HERO_NAME = "Madrigal"
+
+
+fun main() {
+
+    println("The hero announces her presence to the sword.")
+
+    println(HERO_NAME)
+    var playerLevel = 4
+    println(playerLevel)
+
+    println("The hero embarks on her journey to locate the enchanted sword.")
+    playerLevel++
+    println(playerLevel)
+}
