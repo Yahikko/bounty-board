d0167f43179508511e5fcac7d34e6c575ce8af35
Yahikko <taczuhiro@mail.ru>
2023-07-21 21:45:38 +0300

    if/else version of control flow

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,45 @@
-
 const val HERO_NAME = "Madrigal"
-
 
 fun main() {
 
-    println("The hero announces her presence to the sword.")
+    println("The hero announces her presence to the world.")
 
     println(HERO_NAME)
+
     var playerLevel = 4
     println(playerLevel)
 
-    println("The hero embarks on her journey to locate the enchanted sword.")
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+
+    val quest = if (playerLevel == 1) {
+        "Meet Mr. Bubbles in the land of soft things."
+    } else if (playerLevel in 2..5) {
+        // Проверить возможность дипломатического решения
+        val canTalkToBarbarians = !hasAngeredBarbarians &&
+                (hasBefriendedBarbarians || playerClass == "barbarian")
+        if (canTalkToBarbarians) {
+            "Convince the barbarians to call off their invasion."
+        } else {
+            "Save the town from the barbarian invasions."
+        }
+    } else if (playerLevel == 6) {
+        "Locate the enchanted sword."
+    } else if (playerLevel == 7) {
+        "Recover the long-lost artifact of creation."
+    } else if (playerLevel == 8) {
+        "Defeat Nogartse, bringer of death and eater of worlds."
+    } else {
+        "There are no quests right now."
+    }
+
+    println("The hero approaches the bounty board. It reads:")
+    println(quest)
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
     playerLevel++
     println(playerLevel)
 }
